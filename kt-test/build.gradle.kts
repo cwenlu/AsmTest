cd38d7ae079975aeb42987c37dda414a3002a38f
wlchen <cwenlu@sina.cn>
2022-07-01 17:51:05 +0800

    AGP7.x 环境搭建，解决pluginManagement方式使用插件

---
+++ kt-test/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 32
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("com.google.android.material:material:1.4.0")
+}
+
+androidComponents {
+    val debug = selector().withBuildType("debug")
+    beforeVariants(debug){
+        println("debug with variant : ${it.name}")
+    }
+
+}
+
+androidComponents{
+    finalizeDsl {
+        it.buildTypes.create("extra")
+    }
+    beforeVariants{
+        println("all with variant : ${it.name}")
+    }
+}
