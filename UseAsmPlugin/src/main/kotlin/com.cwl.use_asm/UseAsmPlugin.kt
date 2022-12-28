72c56a9ae1ab80b9144e85953399cccaf75352fe
wlchen <cwenlu@sina.cn>
2022-12-28 17:37:56 +0800

    添加笔记

--- UseAsmPlugin/src/main/kotlin/com.cwl.use_asm/UseAsmPlugin.kt
+++ UseAsmPlugin/src/main/kotlin/com.cwl.use_asm/UseAsmPlugin.kt
@@ -11,6 +11,31 @@ import org.gradle.api.Project
  */
 class UseAsmPlugin:Plugin<Project> {
     override fun apply(target: Project) {
-        println("kooooooo")
+
     }
 }
+
+//AppPlugin
+//与com.android.application一起应用的插件,可判断是否是android app
+
+//LibraryPlugin
+//与com.android.library一起应用的插件,可判断是否是android lib
+
+//AndroidComponentsExtension
+//Android Gradle 插件相关组件的通用扩展。 每个组件都有一个类型，如应用程序或库，并且将具有与特定组件类型相关的方法的专用扩展
+
+//ApplicationAndroidComponentsExtension
+//Android Application Gradle Plugin 组件的扩展。这是应用 com.android.application 插件时的 androidComponents 块
+//只有Android Gradle插件才能在com.android.build.api.variant中创建接口实例。
+
+//LibraryAndroidComponentsExtension
+//Android Library Gradle 插件组件的扩展。这是应用 com.android.library 插件时的 androidComponents 块
+//只有Android Gradle插件才能在com.android.build.api.variant中创建接口实例
+
+//project.plugins.withType(AppPlugin::class.java) {
+//    val extension = project.extensions.getByName("androidComponents") as ApplicationAndroidComponentsExtension
+//}
+//
+//project.plugins.withType(LibraryPlugin::class.java) {
+//    val extension = project.extensions.getByName("androidComponents") as LibraryAndroidComponentsExtension
+//}
