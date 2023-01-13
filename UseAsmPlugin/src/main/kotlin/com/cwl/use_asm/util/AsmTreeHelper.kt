decf2a500d5020022f385ebab6e1f15a6dd3208b
wlchen <cwenlu@sina.cn>
2023-01-14 00:52:30 +0800

    完善一些帮助类

--- UseAsmPlugin/src/main/kotlin/com/cwl/use_asm/util/AsmTreeHelper.kt
+++ UseAsmPlugin/src/main/kotlin/com/cwl/use_asm/util/AsmTreeHelper.kt
@@ -1,5 +1,7 @@
 package com.cwl.use_asm.util
 
+import org.objectweb.asm.Opcodes
+import org.objectweb.asm.Type
 import org.objectweb.asm.tree.InvokeDynamicInsnNode
 import org.objectweb.asm.tree.MethodNode
 
@@ -9,6 +11,12 @@ import org.objectweb.asm.tree.MethodNode
  * @Description Asm Tree api 帮助类
  */
 
+
+val MethodNode.isStatic: Boolean
+    get() = access and Opcodes.ACC_STATIC != 0
+
+val MethodNode.isInitMethod: Boolean
+    get() = name == "<init>"
 
 /**
  * 组合name+desc 可以用于唯一定位方法
@@ -34,14 +42,82 @@ fun MethodNode.hasAnnotation(desc: String): Boolean {
  * @return List<InvokeDynamicInsnNode>
  */
 fun MethodNode.filterLambda(predicate: (InvokeDynamicInsnNode) -> Boolean): List<InvokeDynamicInsnNode> {
-    instructions?: return emptyList()
+    instructions ?: return emptyList()
     val dynamicList = mutableListOf<InvokeDynamicInsnNode>()
     instructions.forEach {
-        if(it is InvokeDynamicInsnNode){
-            if(predicate(it)){
+        if (it is InvokeDynamicInsnNode) {
+            if (predicate(it)) {
                 dynamicList.add(it)
             }
         }
     }
     return dynamicList
+}
+
+/**
+ * 根据指定[predicate]获取参数索引
+ * @receiver MethodNode
+ * @param predicate Function1<Array<Type>, Int>
+ * @return Int
+ */
+fun MethodNode.argumentIndexOf(predicate: (Array<Type>) -> Int): Int {
+    val argumentTypes = Type.getArgumentTypes(desc)
+    return predicate(argumentTypes)
+}
+
+/**
+ * 返回第一个符合[desc]的参数索引
+ * @receiver MethodNode
+ * @param desc String
+ */
+fun MethodNode.argumentIndexOfFirst(desc: String): Int {
+    return argumentIndexOf {
+        it.indexOfFirst {
+            it.descriptor == desc
+        }
+    }
+}
+
+/**
+ * 获取指定[argumentIndex]的slot位置
+ * @receiver MethodNode
+ * @param argumentIndex Int
+ * @param argumentTypes Array<Type>
+ * @return Int 错误返回-1 正确返回 >=0
+ */
+fun MethodNode.slotIndex(
+    argumentIndex: Int,
+    argumentTypes: Array<Type> = Type.getArgumentTypes(desc)
+): Int {
+    if (argumentIndex < 0 || argumentIndex >= argumentTypes.size) return -1
+    return if (argumentIndex == 0) {
+        if (isStatic) 0 else 1
+    } else {
+        slotIndex(argumentIndex - 1, argumentTypes) + argumentTypes[argumentIndex - 1].size
+    }
+}
+
+/**
+ * 获取第一个满足[desc]参数的slot
+ * @receiver MethodNode
+ * @param desc String
+ * @return Int
+ */
+fun MethodNode.slotIndexOfFirstDesc(desc: String): Int {
+    val argumentTypes = Type.getArgumentTypes(this.desc)
+    val index = argumentTypes.indexOfFirst {
+        it.descriptor == desc
+    }
+    return slotIndex(index)
+}
+
+/**
+ * 生成一个方法中指定[type]该使用的slot
+ * @receiver MethodNode
+ * @param type Type
+ * @return Int
+ */
+fun MethodNode.newSlotIndex(type: Type): Int {
+    val argumentTypes = Type.getArgumentTypes(desc)
+    return slotIndex(argumentTypes.size - 1, argumentTypes) + type.size
 }
--- app/src/main/java/com/example/asmtest/Test.java
+++ app/src/main/java/com/example/asmtest/Test.java
@@ -6,22 +6,20 @@ package com.example.asmtest;
  * @Description
  */
 public class Test {
-    void test() {
-        //long start = System.currentTimeMillis();
-        //long end = System.currentTimeMillis();
-        //long dis = end - start;
-        //if (dis <= 500) {
-        //    StringBuilder sb = new StringBuilder();
-        //    sb.append("--> execution time : (");
-        //    sb.append(dis);
-        //    String s = sb.toString();
-        //    System.out.println(s);
-        //}
-        long startTime = System.currentTimeMillis();
-        long endTime = System.currentTimeMillis();
-        long time = endTime - startTime;
-        if (time > 500) {
-            System.out.println("程序运行时间： " + time + "ms");
-        }
+    public String qw;
+    public long qe;
+
+    Test(WW w) {
+        qw = "waaa";
+        qe = 3142L;
+
     }
+
+    static void gg(double d, String a) {
+        String m = d + a;
+    }
+}
+
+class WW {
+
 }
